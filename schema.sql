4da691cabceadc2703584d263cd7080d93f89592
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-23 16:06:26 +0100

    refactor schema

--- schema.sql
+++ schema.sql
@@ -3,11 +3,10 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY, 
-    name VARCHAR (20), 
-    date_of_birth DATE, 
-    escape_attempts INT, 
-    neutered BOOL, 
-    weight_kg DECIMAL (5,2), 
-    PRIMARY KEY(id)
-    );
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(10,2)
+);
